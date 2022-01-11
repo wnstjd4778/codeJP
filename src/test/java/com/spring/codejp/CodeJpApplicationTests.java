0f8face00712c221216e6175f38b0f783147d248
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2022-01-11 09:27:11 +0900

    #1 inital commit

---
+++ src/test/java/com/spring/codejp/CodeJpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.codejp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeJpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
